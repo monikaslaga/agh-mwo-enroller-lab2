a6d951893d9719ea0290ce59461ec932d5572e9f
monikaslaga <slaga.monika@gmail.com>
2023-04-15 14:19:42 +0200

    1

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,8 +2,10 @@ package com.company.enroller.persistence;
 
 import com.company.enroller.model.Participant;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 
 import java.util.Collection;
 
@@ -19,6 +21,22 @@ public class ParticipantService {
 	public Collection<Participant> getAll() {
 		return connector.getSession().createCriteria(Participant.class).list();
 	}
+
+	public Collection<Participant> getAll(String sortBy, String sortOrder, String key) {
+		String hql = "FROM Participant WHERE login LIKE :login";
+		if (sortBy.equals("login")) {
+			hql += " ORDER BY login ";
+			if (sortOrder.equals("ASC") || sortOrder.equals("DESC")) {
+				hql += " " + sortOrder;
+			}
+		}
+		Query query = connector.getSession().createQuery(hql);
+		query.setParameter("login", "%"+ key + "%");
+		return query.list();
+	}
+
+
+
 
 	public Participant findByLogin(String login) {
 		return connector.getSession().get(Participant.class, login);
